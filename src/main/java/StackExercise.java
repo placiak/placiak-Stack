98777f0d3e5f95ed29c9cf64442f8681ee6adfd8
placiak <gerlach.tatras@gmail.com>
2023-02-08 22:08:25 +0000

    Completed Stack Exercise

--- src/main/java/StackExercise.java
+++ src/main/java/StackExercise.java
@@ -1,3 +1,4 @@
+import java.util.ArrayDeque;
 import java.util.Deque;
 /**
  * Stacks are LIFO - last in, first out, like a stack of plates or books, or the puzzle game "Towers of Hanoi".
@@ -20,7 +21,8 @@ public class StackExercise {
      * @return a newly instantiated Deque. Deques implement both stack and queue behavior.
      */
     public Deque<String> createNewStack(){
-        return null;
+        ArrayDeque<String> dq = new ArrayDeque<String>();
+        return dq;
     }
 
     /**
@@ -28,7 +30,7 @@ public class StackExercise {
      * @return the number of elements in the deque.
      */
     public int getSize(Deque<String> stack){
-        return 0;
+        return stack.size();
     }
 
     /**
@@ -38,6 +40,7 @@ public class StackExercise {
      * @param item a String to be added to the deque.
      */
     public void addToTopOfStack(Deque<String> stack, String item){
+        stack.add(item);
     }
 
     /**
@@ -47,7 +50,8 @@ public class StackExercise {
      * @return the value popped from the top of the deque.
      */
     public String removeFromTopOfStack(Deque<String> stack){
-        return "";
+        String a = stack.removeLast();
+        return a;
     }
     /**
      * Peek the value at the top of a deque.
@@ -57,6 +61,6 @@ public class StackExercise {
      * @return the value peeked from the top of the deque.
      */
     public String getTopOfStackWithoutRemoving(Deque<String> stack){
-        return "";
+        return stack.getLast();
     }
 }
